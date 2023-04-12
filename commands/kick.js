e9a6555b32585a5f575d76a7fd2f88e86d19b3f0
Flo <floda854@gmail.com>
2023-04-12 15:53:11 +0200

    added kick functionality + other minor changes

--- commands/kick.js
+++ commands/kick.js
@@ -1,6 +1,5 @@
 const { createEmbed } = require("../utils/embed")
 const { Colors, PermissionsBitField } = require("discord.js")
-const { create } = require("../utils/commandList")
 const { getEmoji } = require("../utils/misc")
 const commandFunctions = require("../utils/command")
 
@@ -10,8 +9,9 @@ module.exports = {
     category: "STAFF",
     command: "kick",
 
-    arguments: "<user>",
+    arguments: "<user> <reason>",
     min_args: 1,
+    max_args: 2,
 
     required_permissions: [
         PermissionsBitField.Flags.ModerateMembers
@@ -29,35 +29,56 @@ module.exports = {
             let user = message.mentions.members.first()
             if (user) {
                 if (user.id != message.author.id) {
-                    if (!user.bot) {
+                    if (!user.user.bot) {
                         let allowed = true
                         for (let i = 0; i < this.required_permissions.length; i++) {
                             const permission_bit = this.required_permissions[i];
-                            if (commandFunctions.hasPermission(message.member, permission_bit)) allowed = false
+                            if (commandFunctions.hasPermission(message.member, permission_bit) && message.member.roles.highest.position > user.roles.highest.position) allowed = false
                         }
                         if (!allowed) {
-                            let embed = createEmbed({
-                                title: `${getEmoji("failed")} Missing Permissions`,
+                            let embed_missingperms = createEmbed({
+                                description: `${getEmoji("failed")} Missing Permissions`,
                                 color: Colors.Red,
                                 description: `You are not allowed to kick ${user}.`,
                                 timestamp: true
                             })
-                            return message.reply({ embeds: [embed] })
+                            return await message.reply({ embeds: [embed_missingperms] })
                         }
-
+                        let embed_final = createEmbed({
+                            title: `${getEmoji("check")} Successfully kicked member.`,
+                            description: `${user} has been kicked.`,
+                            color: Colors.Green,
+                            timestamp: true,
+                            fields: [
+                                { name: "Reason", value: arg.rawargs.slice(user.id.length + 3) || "No reason provided" }
+                            ]
+                        })
+                        let guild_string = "`" + `${message.guild.name}` + "`"
+                        let embed_kicked = createEmbed({
+                            title: `${getEmoji("warn")} You've been kicked.`,
+                            description: `You've been kicked from ${guild_string}.`,
+                            color: Colors.Orange,
+                            timestamp: true,
+                            fields: [
+                                { name: "Reason", value: arg.rawargs.slice(user.id.length + 3) || "No reason provided" }
+                            ]
+                        })
+                        await user.send({ embeds: [embed_kicked] })
+                        await user.kick(arg.rawargs.slice(user.id.length + 3) || "No reason provided")
+                        return await message.reply({ embeds: [embed_final] })
                     } else {
-                        let error_embed = createEmbed({
+                        let embed_kickb = createEmbed({
                             description: `${getEmoji("failed")} You can't kick bots with this command!`,
                             color: Colors.Red,
                         })
-                        return message.reply({ embeds: [error_embed] })
+                        return await message.reply({ embeds: [embed_kickb] })
                     }
                 } else {
-                    let error_embed = createEmbed({
+                    let embed_kicky = createEmbed({
                         description: `${getEmoji("failed")} You can't kick yourself from this server!`,
                         color: Colors.Red,
                     })
-                    return message.reply({ embeds: [error_embed] })
+                    return await message.reply({ embeds: [embed_kicky] })
                 }
             } else {
                 let invalid_user = isNaN(arg.args[1].replace("<@", "").replace(">", "")) ? "`" + arg.args[1] + "`" : `${arg.args[1] }`
@@ -65,7 +86,7 @@ module.exports = {
                     description: `${getEmoji("error")} Unable to find ${invalid_user} in this server.`,
                     color: Colors.Red,
                 })
-                return message.reply({ embeds: [error_embed] })
+                return await message.reply({ embeds: [error_embed] })
             }
         } catch (e) {
             commandFunctions.sendErrorMessage(e, arg.message || arg)
